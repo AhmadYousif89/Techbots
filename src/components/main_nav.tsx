8821c3a27f57eff249f968f41faad5c734b7bec0
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-27 22:48:05 +0300

    Add MainNav component

---
+++ src/components/main_nav.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+import { Button } from './ui/button';
+
+export function MainNav() {
+  return (
+    <nav className='hidden lg:flex items-center'>
+      <ul className='flex items-center space-x-6'>
+        <li>
+          <Link href='/'>
+            <Button variant={'link'} className='text-secondary'>
+              Home
+            </Button>
+          </Link>
+        </li>
+        <li>
+          <Link href='/products'>
+            <Button variant={'link'} className='text-secondary'>
+              Products
+            </Button>
+          </Link>
+        </li>
+        <li>
+          <Link href='/blogs'>
+            <Button variant={'link'} className='text-secondary'>
+              Blogs
+            </Button>
+          </Link>
+        </li>
+        <li>
+          <Link href='#'>
+            <Button variant={'link'} className='text-secondary'>
+              About
+            </Button>
+          </Link>
+        </li>
+        <li>
+          <Link href='#'>
+            <Button variant={'link'} className='text-secondary'>
+              Contact
+            </Button>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
