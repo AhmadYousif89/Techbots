cdc1e368fb4bad9c7846b8e79688e88a36da699a
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-27 22:41:51 +0300

    First commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co'
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com'
+      }
+    ]
+  }
+};
 
 export default nextConfig;
