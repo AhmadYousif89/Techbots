6ad50a8ab4affb39e54157aef27320e005578a33
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-04 12:35:39 +0300

    Update component name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import { HomeSlider } from '@/components/slider';
 import { FeaturedProducts } from '@/components/products/featured_products';
-import { FeaturedBlogs } from '@/components/blogs/featured_blogs';
+import { LatestBlogs } from '@/components/blogs/featured_blogs';
 
 export default function Home() {
   return (
-    <main className='min-h-svh'>
+    <main className='min-h-svh max-w-screen-xl mx-auto'>
       <HomeSlider />
       <FeaturedProducts />
-      <FeaturedBlogs />
+      <LatestBlogs />
     </main>
   );
 }
