9bbac2be0621b7c36f7aae37513583f42c24de85
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-04 12:43:13 +0300

    Add UserButton to Header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,8 +3,12 @@ import { SideMenu } from './side_menu';
 import { CartMenu } from './cart/cart_menu';
 import { MainNav } from './main_nav';
 import { WishListMenu } from './wishlist/wishlist_menu';
+import { auth } from '@clerk/nextjs/server';
+import { UserButton } from '@clerk/nextjs';
 
 export function Header() {
+  const { userId } = auth();
+
   return (
     <header className='sticky top-0 z-20 h-20 px-8 bg-foreground/85 backdrop-blur-sm'>
       <div className='flex items-center justify-between max-w-screen-xl h-full mx-auto'>
@@ -14,6 +18,9 @@ export function Header() {
         <div className='flex items-center gap-4 lg:gap-8'>
           <WishListMenu />
           <CartMenu />
+          <div className='ring-1 ring-input rounded-full ring-offset-1 size-7 grid place-content-center bg-muted-foreground'>
+            {userId && <UserButton />}
+          </div>
         </div>
       </div>
     </header>
