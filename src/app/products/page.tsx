24d2967185fee50f0a99e538eb40c1a0742db162
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-04 12:42:29 +0300

    Refactor products page and home slider components

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,89 @@
-import { ProductsLayout } from '@/components/products/products_layout';
+import { Product } from '@/lib/types';
+import type { Category } from '@/lib/store';
+import { getProductsByCategory, getLocalProducts } from '@/lib/actions';
 
-export default function ProductsPage() {
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator
+} from '@/components/ui/breadcrumb';
+import { Card } from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
+import { ProductGrid } from '@/components/products/product_grid';
+import { FilterProducts } from '@/components/products/filters/filter_products';
+import { capitalizeString } from '@/lib/utils';
+
+type PageProps = {
+  searchParams: { [key: string]: string | Category | undefined };
+};
+
+export default async function ProductsPage({ searchParams }: PageProps) {
+  let products = await getLocalProducts();
+  let totalProducts = products.length;
+
+  // Handle products filtering
+  let productsByCategory: Product[] = [];
+  let category = searchParams['category'] ?? '';
+  if (category) {
+    productsByCategory = await getProductsByCategory(category as Category);
+    products = productsByCategory;
+    totalProducts = products.length;
+  }
+  // Handle products pagination
+  const page = searchParams['page'] ?? '1';
+  const limitPerPage = searchParams['limit'] ?? '8';
+  const gridSize = searchParams['grid'] ?? '';
+  const totalPages = Math.ceil(totalProducts / +limitPerPage);
+  const start = (+page - 1) * +limitPerPage;
+  const end = start + +limitPerPage;
+  const hasPrevPage = start > 0;
+  const hasNextPage = end < totalProducts;
+  const paginatedProducts = products.slice(start, end);
+
   return (
-    <main className='min-h-screen'>
-      <ProductsLayout />
+    <main className='min-h-screen max-w-screen-xl mx-auto bg-background'>
+      <Card className='py-8 rounded-none border-0 shadow-none'>
+        <div className='px-2 md:px-4 mb-4 flex items-center justify-between'>
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className='text-xs'>
+                <BreadcrumbLink href='/'>Home</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem className='text-xs'>
+                <BreadcrumbLink href='/products'>Shop</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbPage className='text-xs text-muted-foreground font-semibold'>
+                  {capitalizeString(category)}
+                </BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+
+          <div className='text-xs font-medium text-muted-foreground'>
+            <p>
+              Showing {start + 1} - {end} of {totalProducts} results
+            </p>
+          </div>
+        </div>
+        <Separator />
+        <FilterProducts searchParams={searchParams} />
+        <ProductGrid
+          products={paginatedProducts}
+          hasPrevPage={hasPrevPage}
+          hasNextPage={hasNextPage}
+          totalPages={totalPages}
+          limit={limitPerPage}
+          category={category}
+          gridSize={gridSize}
+          page={+page}
+        />
+      </Card>
     </main>
   );
 }
--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,56 +1,83 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
 import Autoplay from 'embla-carousel-autoplay';
 
 import { Button } from './ui/button';
-import { Carousel, CarouselContent, CarouselItem } from './ui/carousel';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from './ui/carousel';
 
 const slides = [
   {
-    id: 1,
     url: '/products?category=laptops',
-    // title: 'Shop the latest laptops collection',
+    title:
+      'Get high performance, responsiveness, and long battery life with the latest laptop collection',
     img: 'https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   },
   {
-    id: 2,
-    url: '/products?category=cellPhones',
-    // title: 'Shop the latest mobiles flagship',
-    img: 'https://images.unsplash.com/photo-1580327332925-a10e6cb11baa?q=80&w=2014&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    url: '/products?category=mobiles',
+    title: "Don't miss out on the latest mobile phones, tablets, and accessories",
+    img: 'https://m.media-amazon.com/images/I/711F6T6aySL.jpg'
   },
   {
-    id: 3,
-    url: '/products?category=gaming',
-    // title: 'Shop the latest gaming consoles',
+    url: '/products?category=headphones',
+    title:
+      'Experience the best sound quality with our latest collection of headsets and earphones',
     img: 'https://images.unsplash.com/photo-1567928513899-997d98489fbd?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    url: '/products?category=watches',
+    title: 'Stay connected and track your fitness with our latest smartwatches',
+    img: 'https://m.media-amazon.com/images/I/719qgNv-ubL.jpg'
+  },
+  {
+    url: '/products?category=gaming',
+    title: 'Get the best gaming experience with our latest gaming accessories',
+    img: 'https://m.media-amazon.com/images/I/71qhLg1DPVL._AC_SL1500_.jpg'
   }
 ];
 
 export function HomeSlider() {
   return (
-    <Carousel plugins={[]} className='max-w-screen-xl lg:mx-auto'>
-      <CarouselContent className='ml-0'>
-        {slides.map((slide, index) => (
-          <CarouselItem key={index} className='relative h-[calc(100svh-80px)]'>
-            <Image
-              fill
-              src={slide.img}
-              alt={'product image'}
-              className='max-w-screen-xl object-cover'
-            />
-            <Link
-              href={slide.url}
-              className='absolute bottom-10 left-1/2 -translate-x-1/2'>
-              <Button
-                size={'lg'}
-                variant={'outline'}
-                className='bg-foreground/50 text-background'>
-                Shop Now
-              </Button>
-            </Link>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-    </Carousel>
+    <section>
+      <Carousel
+        opts={{ loop: true }}
+        plugins={[Autoplay({ stopOnInteraction: false })]}
+        className='relative group'>
+        <CarouselContent className='ml-0'>
+          {slides.map((slide, index) => (
+            <CarouselItem
+              key={index}
+              className='relative h-[calc(100svh-80px)] grid lg:grid-cols-2'>
+              <Image
+                fill
+                src={slide.img}
+                alt={'product image'}
+                className='max-w-screen-xl sm:object-cover xl:object-fill lg:col-[2]'
+              />
+              <div className='absolute z-10 bottom-0 h-1/2 w-full sm:h-full sm:w-1/2 left-0 lg:left-2 grid place-content-center gap-4 sm:gap-8 lg:gap-12 text-center bg-gradient-to-b from-primary/50 to-primary/90 xl py-8 px-4 lg:px-16 backdrop-blur-sm'>
+                <p className='text-secondary p-4 rounded text-lg lg:text-xl xl:text-2xl'>
+                  {slide.title}
+                </p>
+                <Button
+                  size={'lg'}
+                  variant={'outline'}
+                  className='place-self-center bg-foreground/50 text-background'>
+                  <Link href={slide.url}>Shop Now</Link>
+                </Button>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className='hidden group-hover:inline-flex left-5 z-10 lg:rounded border-input/20 size-12 lg:h-1/2 bg-transparent *:text-secondary lg:*:size-5 *:hover:text-primary' />
+        <CarouselNext className='hidden group-hover:inline-flex right-5 z-10 lg:rounded border-input/20 size-12 lg:h-1/2 bg-transparent *:text-secondary lg:*:size-5 *:hover:text-primary' />
+      </Carousel>
+    </section>
   );
 }
