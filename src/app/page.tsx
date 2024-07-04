dd68174e2e13e3b70d373b4cde244cd1900ae26b
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-04 19:27:24 +0300

    Add popular items and tech deals slides to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import { HomeSlider } from '@/components/slider';
 import { FeaturedProducts } from '@/components/products/featured_products';
 import { LatestBlogs } from '@/components/blogs/featured_blogs';
+import { PopularItemsSlide } from '@/components/popular_items_slide';
+import { TechDealsSlide } from '@/components/tech_deals_slide';
 
 export default function Home() {
   return (
     <main className='min-h-svh max-w-screen-xl mx-auto'>
       <HomeSlider />
       <FeaturedProducts />
+      <PopularItemsSlide />
+      <TechDealsSlide />
       <LatestBlogs />
     </main>
   );
