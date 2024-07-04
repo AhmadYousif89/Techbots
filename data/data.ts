1eed02f93ca2f689cf8d7e19d350910493da5739
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-05 01:20:55 +0300

    Update blog URLs

--- data/data.ts
+++ data/data.ts
@@ -34,7 +34,7 @@ export const blogsData = [
     image:
       'https://images.unsplash.com/photo-1624701928517-44c8ac49d93c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     title: 'Your Guide to Choosing a Graphics Card',
-    url: 'https://example.com/choosing-a-graphics-card',
+    url: 'https://www.digitaltrends.com/computing/gpu-buying-guide/',
     description:
       'Learn how to select the best graphics card for your gaming or professional needs, with tips on performance, compatibility, and budget considerations.'
   },
@@ -43,7 +43,7 @@ export const blogsData = [
     image:
       'https://images.unsplash.com/photo-1600336757481-6185c4be6ff6?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     title: 'Top 5 Motherboards for 2024',
-    url: 'https://example.com/top-5-motherboards-2024',
+    url: 'https://www.tomshardware.com/best-picks/best-motherboards',
     description:
       'Discover the best motherboards of 2024, featuring top-notch performance, advanced features, and compatibility with the latest processors.'
   },
@@ -52,7 +52,7 @@ export const blogsData = [
     image:
       'https://images.unsplash.com/photo-1610415390571-0462860c54c3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     title: 'SSD vs HDD: Which One Should You Choose?',
-    url: 'https://example.com/ssd-vs-hdd',
+    url: 'https://www.crucial.com/articles/about-ssd/ssd-vs-hdd',
     description:
       'Compare the benefits and drawbacks of SSDs and HDDs to make an informed decision on which storage solution is right for your needs.'
   },
@@ -61,7 +61,7 @@ export const blogsData = [
     image:
       'https://images.unsplash.com/photo-1706102817449-9a22be1e02c1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Y3VzdG9tJTIwcGN8ZW58MHx8MHx8fDA%3D',
     title: 'Building Your Own PC: A Step-by-Step Guide',
-    url: 'https://example.com/building-your-own-pc',
+    url: 'https://www.pcmag.com/how-to/how-to-build-a-pc-the-ultimate-beginners-guide',
     description:
       'Follow this comprehensive guide to building your own PC, from selecting components to assembling and configuring your system.'
   },
@@ -70,7 +70,7 @@ export const blogsData = [
     image:
       'https://images.unsplash.com/photo-1616763355548-1b606f439f86?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     title: 'The Best Monitors for Gaming and Productivity',
-    url: 'https://example.com/best-monitors-2024',
+    url: 'https://www.rtings.com/monitor/reviews/best/monitors',
     description:
       'Explore the top monitors available in 2024, perfect for both gaming enthusiasts and professionals seeking high productivity.'
   }
