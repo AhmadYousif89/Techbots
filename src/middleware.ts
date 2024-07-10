2d76ea3dfd558735c7ab8f1304c2876d213372f3
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-11 00:59:57 +0300

    Update protected routes in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher(['/admin(.*)']);
+const isProtectedRoute = createRouteMatcher(['/orders(.*),/admin(.*),/checkout(.*)']);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
