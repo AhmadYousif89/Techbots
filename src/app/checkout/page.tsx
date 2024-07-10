5cbd1c106be798ed643a670d492747eadde51531
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-11 01:09:34 +0300

    Add checkout page and update protected routes

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1 className='sr-only'>Checkout terminal</h1>;
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher(['/orders(.*),/admin(.*),/checkout(.*)']);
+const isProtectedRoute = createRouteMatcher(['/orders(.*),/checkout(.*)']);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
