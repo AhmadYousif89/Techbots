ecfef885af41c9350e39ad3dcd7a31ae5804c392
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-12 06:08:40 +0300

    Fix import order in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import { HomeSlider } from '@/components/slider';
 import { FeaturedProducts } from '@/app/products/_components/featured_products';
 import { LatestBlogs } from '@/app/blogs/_components/featured_blogs';
 import { PopularItemsSlide } from '@/components/popular_items_slide';
 import { TechDealsSlide } from '@/components/tech_deals_slide';
+import { HomeSlider } from '@/components/slider';
 
 export default function Home() {
   return (
