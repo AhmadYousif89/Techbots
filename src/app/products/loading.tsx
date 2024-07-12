f48fbe81d93ee1b5945bcc7cd5981fcd4100a794
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-12 06:09:02 +0300

    Add Loading component

---
+++ src/app/products/loading.tsx
@@ -0,0 +1,11 @@
+import { Loader } from 'lucide-react';
+
+export default function Loading() {
+  return (
+    <div className='flex items-center justify-center min-h-screen bg-background max-w-screen-xl mx-auto'>
+      <div className='flex flex-col items-center animate-pulse overflow-hidden'>
+        <Loader className='size-20 md:size-28 animate-[spin_2s_linear_infinite]'></Loader>
+      </div>
+    </div>
+  );
+}
