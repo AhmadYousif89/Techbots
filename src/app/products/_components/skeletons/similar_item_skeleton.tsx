815e6f291bd9ce965cd25ab187724e0e2cb248fe
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-15 02:19:57 +0300

    Refactor skeleton components for similar items and main page slide

--- src/app/products/_components/skeletons/similar_item_skeleton.tsx
+++ src/app/products/_components/skeletons/similar_item_skeleton.tsx
@@ -1,34 +1,35 @@
 import { Card } from '@/components/ui/card';
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious
-} from '@/components/ui/carousel';
 import { Skeleton } from '@/components/ui/skeleton';
+import { Carousel, CarouselItem, CarouselContent } from '@/components/ui/carousel';
 
 export function SimilarItemSkeleton() {
   return (
-    <Carousel
-      className='mx-auto max-w-[min(320px,80vw)] sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-screen-lg'
-      opts={{ dragFree: true, align: 'start' }}>
-      <CarouselContent>
-        {Array.from({ length: 10 }).map((_, index) => (
-          <CarouselItem key={index} className='basis-48 grid pb-4'>
-            <Card className='p-2 grid min-h-44 shadow-sm'>
-              <Skeleton className='min-h-36 mb-2 bg-input/80' />
-              <Skeleton className='w-full h-4 my-2  bg-input/50' />
-              <div className='flex justify-between gap-4 items-center'>
-                <Skeleton className='w-full h-2.5  bg-input/70' />
-                <Skeleton className='w-full h-2.5 bg-input/80' />
-              </div>
-            </Card>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious className='top-full translate-y-1/2 left-0' />
-      <CarouselNext className='top-full translate-y-1/2 right-0' />
-    </Carousel>
+    <div className='bg-background p-8 '>
+      <Skeleton className='w-1/2 h-4 bg-input/80 mb-8' />
+      <div>
+        <Carousel
+          className='mx-auto max-w-[80vw] xl:max-w-screen-lg'
+          opts={{ dragFree: true, align: 'start' }}>
+          <CarouselContent>
+            {Array.from({ length: 10 }).map((_, index) => (
+              <CarouselItem key={index} className='basis-48 grid pb-4'>
+                <Card className='p-2 grid min-h-44 border-muted shadow-sm'>
+                  <Skeleton className='min-h-36 mb-2 bg-input/80' />
+                  <Skeleton className='w-full h-4 my-2  bg-input/70' />
+                  <div className='flex justify-between gap-4 items-center'>
+                    <Skeleton className='w-full h-2.5  bg-input/70' />
+                    <Skeleton className='w-full h-2.5 bg-input/80' />
+                  </div>
+                </Card>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <div className='flex justify-between mt-4'>
+            <Skeleton className='rounded-full bg-input/80 size-8' />
+            <Skeleton className='rounded-full bg-input/80 size-8' />
+          </div>
+        </Carousel>
+      </div>
+    </div>
   );
 }
