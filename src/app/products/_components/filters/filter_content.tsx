ea213c7ff136c7c616839d346db385e76e5a55aa
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-18 03:04:36 +0300

    Add filter content for categories and brands

--- src/app/products/_components/filters/filter_content.tsx
+++ src/app/products/_components/filters/filter_content.tsx
@@ -1,37 +1,93 @@
+import { Suspense } from 'react';
+import prisma from '@/lib/db';
+import { cache } from '@/lib/cache';
 import { Separator } from '@/components/ui/separator';
 import { SearchParams } from '@/app/products/_lib/types';
+import { ApplyFiltersButton } from './apply_filters_button';
 import { FilterContentPrice } from './filter_content_price';
+import { FilterContentBrands } from './filter_content_brand';
 import { FilterContentCategory } from './filter_content_category';
+import { FilterItemsSkeleton } from '../skeletons/filter_items_skeleton';
+
+const day = 60 * 60 * 24;
+
+export const getCategoryList = cache(
+  async () => {
+    const categories = await prisma.product.findMany({
+      select: { category: true, brand: true },
+      orderBy: { category: 'asc' }
+    });
+    const list: string[] = [];
+    for (const item of categories) {
+      if (!list.includes(item.category.toLowerCase())) {
+        list.push(item.category.toLowerCase());
+      }
+    }
+
+    return list;
+  },
+  ['/products', 'getCategoryList'],
+  { revalidate: day }
+);
+
+export const getBrandList = cache(
+  async () => {
+    const brands = await prisma.product.findMany({
+      select: { brand: true },
+      orderBy: { brand: 'asc' }
+    });
+    const list: string[] = [];
+    for (const item of brands) {
+      if (!list.includes(item.brand.toLowerCase())) {
+        if (item.brand.startsWith('Philips')) {
+          list.push('philips');
+          continue;
+        }
+        list.push(item.brand.toLowerCase());
+      }
+    }
+
+    return list;
+  },
+  ['/products', 'getBrandList'],
+  { revalidate: day }
+);
 
 type FilterContentProps = {
   searchParams: SearchParams;
 };
 
 export async function FilterContent({ searchParams }: FilterContentProps) {
-  // const colors = await getColorList();
+  const categories = getCategoryList();
+  const brands = getBrandList();
 
   return (
-    <div className='p-4'>
-      <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-[1fr_1.5fr_1fr]'>
-        {/* Categories */}
-        <section className='mb-8'>
-          <FilterContentCategory />
-        </section>
+    <div className='grid gap-y-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-[1fr_1.5fr_1fr]'>
+      <section className='space-y-8'>
+        <Suspense fallback={<FilterItemsSkeleton />}>
+          <FilterContentCategory data={categories} />
+        </Suspense>
+        <Separator className='sm:w-px sm:h-auto' />
+      </section>
 
-        <Separator className='sm:w-px sm:h-auto sm:col-[2] sm:row-[1]' />
+      <section className='sm:flex flex-row-reverse gap-8 space-y-8'>
+        <Suspense fallback={<FilterItemsSkeleton />}>
+          <div className='flex-1 flex flex-col gap-4'>
+            <FilterContentBrands data={brands} />
+          </div>
+        </Suspense>
+        <Separator className='sm:w-px sm:h-auto' />
+      </section>
 
-        {/* Colors */}
-        <section className='mb-8 mt-4 sm:mt-0 sm:ml-[10%] sm:col-[2] sm:row-[1]'>
-          {/* <FilterContentColors colors={colors} /> */}
-        </section>
-
-        <Separator className='col-span-full lg:w-px lg:h-auto lg:col-[3] lg:row-[1]' />
-
-        {/* Price */}
-        <section className='mb-8 mt-4 lg:mt-0 lg:ml-[10%] col-[1] lg:col-[3] lg:row-[1]'>
+      <Separator className='hidden sm:block col-span-full' />
+      <section className='space-y-8 sm:col-span-full lg:flex lg:flex-row-reverse lg:gap-x-8 lg:col-[3] lg:row-[1]'>
+        <div className='max-w-xl'>
           <FilterContentPrice />
-        </section>
-      </div>
+        </div>
+        <Separator className='lg:w-px lg:h-auto' />
+      </section>
+
+      <ApplyFiltersButton />
     </div>
   );
 }
--- src/app/products/_components/filters/filter_products.tsx
+++ src/app/products/_components/filters/filter_products.tsx
@@ -1,10 +1,6 @@
 import { Filter, Search } from 'lucide-react';
 import { SearchParams } from '@/app/products/_lib/types';
-
-import { SortProducts } from './sort_products';
 import { FilterContent } from './filter_content';
-import { SearchProducts } from './search_products';
-import { ApplyFiltersButton } from './apply_filters_button';
 
 import {
   Accordion,
@@ -18,41 +14,23 @@ type FilterProductsProps = {
   searchParams: SearchParams;
 };
 
-export async function FilterProducts({ searchParams }: FilterProductsProps) {
-  // const cf = searchParams['cf'];
-
+export function FilterProducts({ searchParams }: FilterProductsProps) {
   return (
-    <section aria-label='filters' className='flex items-center justify-between'>
-      <div className='relative flex items-center gap-4 w-full'>
-        <Accordion type='single' collapsible className='flex-grow'>
-          <AccordionItem value='filter' className='data-[state="closed"]:border-b-0'>
-            <AccordionTrigger className='group [&>svg]:hidden flex-grow-0 py-6 pl-2 md:pl-4'>
-              <div className='flex items-center text-xs gap-2 hover:border-transparent'>
-                <Filter className='size-4 group-hover:fill-muted-foreground group-data-[state="open"]:fill-muted-foreground' />
-                <span>Filters</span>
-              </div>
-            </AccordionTrigger>
-            <Separator />
-            <AccordionContent className='p-0'>
-              {/* Filter Content */}
-              <FilterContent searchParams={searchParams} />
-              {/* Filter Content */}
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-
-        <div className='absolute left-20 md:left-24 top-[14px] flex items-center'>
-          <Separator orientation='vertical' className='h-6 mr-2' />
-          <ApplyFiltersButton />
-        </div>
-
-        <div className='absolute right-0 md:right-2 top-[14px]'>
-          <div className='flex items-center gap-4'>
-            <SearchProducts />
-            <SortProducts />
+    <Accordion type='single' collapsible className='flex-grow'>
+      <AccordionItem value='filter' className='data-[state="closed"]:border-b-0'>
+        <AccordionTrigger className='group [&>svg]:hidden flex-grow-0 py-6 pl-4 md:pl-10'>
+          <div className='flex items-center text-xs gap-2 hover:border-transparent'>
+            <Filter className='size-4 group-hover:fill-muted-foreground group-data-[state="open"]:fill-muted-foreground' />
+            <span>Filters</span>
           </div>
-        </div>
-      </div>
-    </section>
+        </AccordionTrigger>
+        <Separator />
+        <AccordionContent className='p-6'>
+          {/* Filter Content */}
+          <FilterContent searchParams={searchParams} />
+          {/* Filter Content */}
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   );
 }
