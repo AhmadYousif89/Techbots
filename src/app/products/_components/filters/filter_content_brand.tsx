9b0aff254215f3edc4ea2a0b125d73c28ad399e0
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-22 01:56:16 +0300

    Refactor brand filter component and update URL parameters

--- src/app/products/_components/filters/filter_content_brand.tsx
+++ src/app/products/_components/filters/filter_content_brand.tsx
@@ -21,53 +21,78 @@ export function FilterContentBrands({ data }: { data: Promise<string[]> }) {
   const router = useRouter();
   const params = useSearchParams();
   const [showMore, setShowMore] = useState(false);
-  const { brand } = extractSearchParams(params.entries());
-  const { selectedBrands, setBrands, clearSelectedBrands } = useFilter(s => s.brands);
+  const { selectedBrands, setBrands, toggleBrands, clearSelectedBrands } = useFilter(
+    s => s.brands
+  );
+  const { page, limit, category, brand, sort, min, max, grid } = extractSearchParams(
+    params.entries()
+  );
 
-  // useEffect(() => {
-  //   if (brand) {
-  //     const brands = brand.includes(',') ? brand.split(',') : [brand];
-  //     setBrands(brands);
-  //   }
-  // }, [brand]);
+  useEffect(() => {
+    if (brand) {
+      console.log('brand: ', brand);
+      setBrands(brand.split(','));
+    }
+  }, [brand, setBrands]);
 
-  console.log('selectedBrands', selectedBrands);
+  const newParams = new URLSearchParams({
+    ...(page && { page }),
+    ...(limit && { limit }),
+    ...(limit && { limit }),
+    ...(category && { cat: category }),
+    ...(sort && { sort }),
+    ...(min && { min }),
+    ...(max && { max }),
+    ...(grid && { grid })
+  });
+
+  const url = () => `/products/?${newParams.toString()}`;
 
   const hasFilterBrand = selectedBrands.length > 0;
-  const end = 24;
+  const end = 30;
   const list = brands.length > end ? brands.slice(0, end) : brands;
 
   const onBrandChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name } = e.target;
-    setBrands([name]);
+    toggleBrands([name]);
     const brands = [...selectedBrands];
     if (!brands.includes(name)) {
       brands.push(name);
       const newBrandQuery = brands.join(',');
-      router.push(`/products?brand=${newBrandQuery}`);
+      router.push(url() + (newBrandQuery ? `&brand=${newBrandQuery}` : ''));
     } else {
       const newBrandQuery = brands.filter(brand => brand !== name).join(',');
-      router.push(`/products?brand=${newBrandQuery}`);
+      router.push(url() + (newBrandQuery ? `&brand=${newBrandQuery}` : ''));
     }
   };
 
   return (
     <>
-      <div className='flex items-center gap-4'>
-        <h3 className='font-medium text-muted-foreground'>Brands</h3>
+      <div className='flex items-center justify-between gap-4'>
+        <h3 className='flex items-center gap-2 font-medium text-muted-foreground'>
+          Brands
+          {selectedBrands.length > 0 && (
+            <small>
+              <strong className='text-blue-500 ring-1 rounded-full aspect-square size-5 inline-grid place-content-center mr-1'>
+                {selectedBrands.length}
+              </strong>
+              total selected
+            </small>
+          )}
+        </h3>
         {hasFilterBrand && (
           <Button
             variant={'link'}
             onClick={() => {
               clearSelectedBrands();
-              router.push('/products');
+              router.push(url());
             }}
             className='gap-1 text-xs py-0 h-auto font-medium text-muted-foreground hover:text-destructive'>
             <ChevronLeft className='size-3' /> Clear
           </Button>
-        )}{' '}
+        )}
       </div>
-      <div className='px-2 grid grid-cols-[repeat(auto-fit,minmax(120px,1fr))] gap-2 space-y-1'>
+      <div className='pl-4 grid grid-cols-[repeat(auto-fit,minmax(120px,auto))] gap-2 space-y-1 xl:grid-cols-[minmax(120px,auto),auto] xl:gap-x-0'>
         {list.map((brand, i) => {
           const key = brand + '-' + i;
           return (
@@ -83,7 +108,7 @@ export function FilterContentBrands({ data }: { data: Promise<string[]> }) {
               <Label
                 htmlFor={key}
                 className='text-xs cursor-pointer hover:text-muted-foreground'>
-                {capitalizeString(brand)}
+                {brand.startsWith('Philips') ? 'Philips' : capitalizeString(brand, false)}
               </Label>
             </div>
           );
@@ -92,7 +117,7 @@ export function FilterContentBrands({ data }: { data: Promise<string[]> }) {
           <Accordion type='single' collapsible className='col-span-full'>
             <AccordionItem value='cats-2' className='border-b-0'>
               <AccordionContent>
-                <div className='grid grid-cols-[repeat(auto-fit,minmax(120px,1fr))] gap-2 space-y-1'>
+                <div className='grid grid-cols-[repeat(auto-fit,minmax(120px,auto))] gap-2 space-y-1 xl:grid-cols-[minmax(120px,auto),auto] xl:gap-x-0'>
                   {brands.slice(end).map((brand, i) => {
                     const key = brand + '-' + i;
                     return (
@@ -108,7 +133,9 @@ export function FilterContentBrands({ data }: { data: Promise<string[]> }) {
                         <Label
                           htmlFor={key}
                           className='text-xs cursor-pointer hover:text-muted-foreground'>
-                          {capitalizeString(brand)}
+                          {brand.startsWith('Philips')
+                            ? 'Philips'
+                            : capitalizeString(brand, false)}
                         </Label>
                       </div>
                     );
@@ -117,7 +144,7 @@ export function FilterContentBrands({ data }: { data: Promise<string[]> }) {
               </AccordionContent>
               <AccordionTrigger
                 onClick={() => setShowMore(!showMore)}
-                className='py-0 pt-4 text-[10px] underline-offset-4 gap-1 justify-center text-muted-foreground hover:text-primary/50'>
+                className='py-0 pt-4 text-xs underline-offset-4 gap-1 justify-center text-muted-foreground hover:text-primary/50'>
                 {showMore ? 'Show Less' : 'Show More'}
               </AccordionTrigger>
             </AccordionItem>
