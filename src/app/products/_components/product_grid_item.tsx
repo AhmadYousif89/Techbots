b672456b70a67cc73d8ccffd3b9a17248eb60ac3
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-22 01:55:54 +0300

    Refactor product grid item component to include additional search parameters

--- src/app/products/_components/product_grid_item.tsx
+++ src/app/products/_components/product_grid_item.tsx
@@ -9,6 +9,7 @@ import { Separator } from '@/components/ui/separator';
 import { AddToCartButton } from '../../cart/_components/add_to_cart_button';
 import { AddToWishlistButton } from '@/components/wishlist/add_to_wishlist_button';
 import { Card, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { cn } from '@/lib/utils';
 
 type Props = {
   searchParams: SearchParams;
@@ -16,28 +17,52 @@ type Props = {
 };
 
 export async function ProductGridItem({ product, searchParams }: Props) {
-  const { page, limit, sort, grid } = extractSearchParams(searchParams);
+  const {
+    page,
+    limit,
+    category: cat,
+    brand,
+    min,
+    max,
+    sort,
+    grid
+  } = extractSearchParams(searchParams);
+  const params = new URLSearchParams({
+    ...(page && { page }),
+    ...(limit && { limit }),
+    ...(cat ? { cat } : {}),
+    ...(brand ? { brand } : {}),
+    ...(sort ? { sort } : {}),
+    ...(min ? { min } : {}),
+    ...(max ? { max } : {}),
+    ...(grid ? { grid } : {})
+  });
   const { asin, rating, mainImage, title, price, category } = product;
   const prodUrl = `/products/${asin}?cat=${category}`;
-  const forceRedirectUrl = `/cart?page=${page}&limit=${limit}&cat=${category}&sort=${sort}&grid=${grid}`;
 
   return (
-    <Card className='grid auto-rows-[150px_3px_1fr_3px_auto] md:auto-rows-[200px_3px_1fr_3px_auto] justify-self-center p-2 shadow-none border-0'>
-      <Link
-        href={prodUrl}
-        className='place-self-center rounded active:border active:border-blue-500'>
+    <Card className='grid auto-rows-[150px_3px_1fr_3px_auto] md:auto-rows-[200px_3px_1fr_3px_auto] justify-self-center p-2 rounded shadow-none border-0 max-w-xs ring-offset-4 active:ring-1 active:ring-muted'>
+      <Link href={prodUrl} className='place-self-center rounded'>
         <Image
           src={mainImage}
           alt={title}
-          width={100}
-          height={100}
-          className='size-28 sm:size-36 p-2 object-contain'
+          width={150}
+          height={150}
+          className={cn(
+            'size-28 object-contain',
+            grid === '3' && 'lg:size-36 p-0',
+            grid === '2' && 'lg:size-44 p-2'
+          )}
         />
       </Link>
       <Separator />
       <CardHeader className='p-0 py-4 justify-center space-y-0'>
         <Link href={prodUrl}>
-          <CardTitle className='text-xs font-medium hover:underline hover:text-blue-700'>
+          <CardTitle
+            className={cn(
+              'text-xs font-medium hover:underline hover:text-blue-700',
+              grid === '2' && 'text-sm'
+            )}>
             {title}
           </CardTitle>
         </Link>
@@ -52,7 +77,7 @@ export async function ProductGridItem({ product, searchParams }: Props) {
           size={'sm'}
           action='BuyNow'
           product={product}
-          forceRedirect={forceRedirectUrl}
+          forceRedirect={`/cart?${params.toString()}`}
         />
         <AddToWishlistButton logoSize={18} product={product} />
       </CardFooter>
