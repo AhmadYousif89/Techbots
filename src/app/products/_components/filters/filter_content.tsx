3a5303e2b4135f09c8f2f4264112a8544f4a59bd
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-22 01:56:27 +0300

    Refactor filter_content.tsx to improve code structure and performance

--- src/app/products/_components/filters/filter_content.tsx
+++ src/app/products/_components/filters/filter_content.tsx
@@ -1,85 +1,29 @@
 import { Suspense } from 'react';
-import prisma from '@/lib/db';
-import { cache } from '@/lib/cache';
+import { extractSearchParams } from '../../_lib/utils';
+import { getBrandList, getCategoryList } from '@/lib/products';
+
 import { Separator } from '@/components/ui/separator';
 import { SearchParams } from '@/app/products/_lib/types';
 import { FilterContentPrice } from './filter_content_price';
 import { FilterContentBrands } from './filter_content_brand';
-import { FilterContentCategory } from './filter_content_category';
 import { FilterItemsSkeleton } from '../skeletons/filter_items_skeleton';
 
-const day = 60 * 60 * 24;
-
-export const getCategoryList = cache(
-  async () => {
-    const categories = await prisma.product.findMany({
-      select: { category: true, brand: true },
-      orderBy: { category: 'asc' }
-    });
-    const list: string[] = [];
-    for (const item of categories) {
-      if (!list.includes(item.category.toLowerCase())) {
-        list.push(item.category.toLowerCase());
-      }
-    }
-
-    return list;
-  },
-  ['/products', 'getCategoryList'],
-  { revalidate: day }
-);
-
-export const getBrandList = async () => {
-  const brands = await prisma.product.findMany({
-    select: { brand: true },
-    orderBy: { brand: 'asc' }
-  });
-
-  const list: string[] = [];
-  for (const item of brands) {
-    if (!list.includes(item.brand)) {
-      if (item.brand.startsWith('Philips')) {
-        list.push('philips');
-        continue;
-      }
-      list.push(item.brand);
-    }
-  }
-
-  return list;
-};
-
-type FilterContentProps = {
-  searchParams: SearchParams;
-};
-
-export async function FilterContent({ searchParams }: FilterContentProps) {
+export async function FilterContent(searchParams: SearchParams) {
+  const { category } = extractSearchParams(searchParams);
   const categories = getCategoryList();
-  const brands = getBrandList();
+  const brands = getBrandList(category);
 
   return (
-    <div className='grid gap-y-8 sm:grid-cols-2 xl:grid-cols-1 xl:max-w-xs'>
-      {/* <section className='space-y-8'>
-        <Suspense fallback={<FilterItemsSkeleton />}>
-          <FilterContentCategory data={categories} />
-        </Suspense>
-        <Separator className='sm:w-px sm:h-auto' />
-      </section> */}
-
+    <div className='grid gap-y-8 sm:grid-cols-[60%,40%] xl:grid-cols-1 xl:max-w-xs'>
       <section className='space-y-8'>
         <Suspense fallback={<FilterItemsSkeleton />}>
-          <div className='flex flex-col gap-4'>
-            <FilterContentBrands data={brands} />
-          </div>
+          <FilterContentBrands data={brands} />
         </Suspense>
         <Separator className='sm:hidden' />
       </section>
 
       <section className='sm:flex sm:flex-row-reverse sm:gap-8 xl:flex-col-reverse'>
-        <div>
-          <FilterContentPrice />
-          {/* <ApplyFiltersButton /> */}
-        </div>
+        <FilterContentPrice />
         <Separator className='hidden sm:block sm:w-px sm:h-auto xl:w-full xl:h-px ' />
       </section>
     </div>
