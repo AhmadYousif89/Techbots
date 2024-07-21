eb0b15ade5aecc1a72e3ffca511a96839938848d
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-22 01:56:01 +0300

    Refactor product grid size component

--- src/app/products/_components/product_grid_size.tsx
+++ src/app/products/_components/product_grid_size.tsx
@@ -12,55 +12,59 @@ export function ProductGridSize() {
   const router = useRouter();
   const params = useSearchParams();
 
-  const { page, limit, category, sort, min, max } = extractSearchParams(params.entries());
+  const { page, limit, category, brand, sort, min, max } = extractSearchParams(
+    params.entries()
+  );
   const newParams = new URLSearchParams({
-    page,
-    limit,
-    cat: category,
-    sort,
-    min,
-    max
+    ...(page && { page }),
+    ...(limit && { limit }),
+    ...(limit && { limit }),
+    ...(category && { cat: category }),
+    ...(brand && { brand }),
+    ...(sort && { sort }),
+    ...(min && { min }),
+    ...(max && { max })
   });
-  const url = (grid: string) => `/products/?${newParams.toString()}&grid=${grid}`;
+  const url = () => `/products/?${newParams.toString()}`;
 
   return (
     <div role='group' className='hidden lg:flex items-center gap-1 col-span-full'>
       <Button
         variant='ghost'
         data-active={params.get('grid') === '2'}
-        className='p-2 border border-transparent hover:bg-transparent data-[active="true"]:border-input data-[active="true"]:shadow-sm'
+        className='p-2 rounded-none ring-1 ring-transparent hover:bg-transparent data-[active="true"]:ring-input data-[active="true"]:shadow'
         onClick={() => {
           if (params.get('grid') === '2') {
-            router.push(url(''));
+            router.push(url());
             return;
           }
-          router.push(url('2'));
+          router.push(url() + '&grid=2');
         }}>
         <Grid2x2 />
       </Button>
       <Button
         variant='ghost'
         data-active={params.get('grid') === '3'}
-        className='p-2 border border-transparent hover:bg-transparent data-[active="true"]:border-input data-[active="true"]:shadow-sm'
+        className='p-2 rounded-none ring-1 ring-transparent hover:bg-transparent data-[active="true"]:ring-input data-[active="true"]:shadow'
         onClick={() => {
           if (params.get('grid') === '3') {
-            router.push(url(''));
+            router.push(url());
             return;
           }
-          router.push(url('3'));
+          router.push(url() + '&grid=3');
         }}>
         <Grid3x3 />
       </Button>
       <Button
         variant='ghost'
         data-active={params.get('grid') === '4'}
-        className='p-2 border border-transparent hover:bg-transparent data-[active="true"]:border-input data-[active="true"]:shadow-sm'
+        className='p-2 rounded-none ring-1 ring-transparent hover:bg-transparent data-[active="true"]:ring-input data-[active="true"]:shadow'
         onClick={() => {
           if (params.get('grid') === '4') {
-            router.push(url(''));
+            router.push(url());
             return;
           }
-          router.push(url('4'));
+          router.push(url() + '&grid=4');
         }}>
         <Grid4x4 />
       </Button>
