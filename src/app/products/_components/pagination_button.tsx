5a490ccf31946f657422907f6f4257a2a039337b
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-24 23:27:40 +0300

    Refactor pagination buttons component

--- src/app/products/_components/pagination_button.tsx
+++ src/app/products/_components/pagination_button.tsx
@@ -1,39 +1,82 @@
-'use client';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { ChevronsLeft, ChevronLeft, ChevronRight, ChevronsRight } from 'lucide-react';
 
-import { useRouter } from 'next/navigation';
-import { useEffect, useRef, ReactNode } from 'react';
-
-import { Button, ButtonProps } from '../../../components/ui/button';
+import { Button } from '@/components/ui/button';
+import { SelectPaginations } from './select_pagination';
 
 type PaginationButtonProps = {
-  href: string;
-  elementId?: string;
-} & ButtonProps;
+  className?: string;
+  page: string;
+  params: string;
+  baseUrl: string;
+  startingPage: number;
+  endingPage: number;
+  totalPages: number;
+  totalCount?: number;
+  hasPrevPage: boolean;
+  hasNextPage: boolean;
+  firstPageUrl: string;
+  prevPageUrl: string;
+  nextPageUrl: string;
+  lastPageUrl: string;
+};
 
-export const PaginationButton = ({
-  href,
-  elementId,
-  ...props
-}: PaginationButtonProps) => {
-  const router = useRouter();
-  const reviewsRef = useRef<HTMLElement | null>(null);
-
-  useEffect(() => {
-    if (elementId) {
-      reviewsRef.current = document.getElementById(elementId) as HTMLElement;
-    }
-  }, [elementId]);
-
-  const handleClick = () => {
-    if (reviewsRef.current) {
-      reviewsRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-    router.push(href);
-  };
-
+export const PaginationButtons = (props: PaginationButtonProps) => {
   return (
-    <Button onClick={handleClick} size={'icon'} variant={'outline'} {...props}>
-      {props.children}
-    </Button>
+    <div className={cn('flex items-center gap-2', props.className)}>
+      {props.totalPages > 2 && (
+        <Link className='grid' href={props.firstPageUrl}>
+          <Button
+            variant={'outline'}
+            className='size-6 p-0 disabled:opacity-25'
+            disabled={!props.hasPrevPage}>
+            <ChevronsLeft className='size-4' />
+          </Button>
+        </Link>
+      )}
+      <Link className='grid' href={props.prevPageUrl}>
+        <Button
+          variant={'outline'}
+          className='size-6 p-0 disabled:opacity-25'
+          disabled={!props.hasPrevPage}>
+          <ChevronLeft className='size-4' />
+        </Button>
+      </Link>
+      <span className='text-xs text-muted-foreground font-semibold'>
+        {props.totalPages <= 2 ? (
+          <>
+            {props.startingPage} / {props.endingPage}
+          </>
+        ) : (
+          <SelectPaginations
+            page={props.page}
+            url={props.baseUrl}
+            params={props.params}
+            startingPage={props.startingPage}
+            endingPage={props.endingPage}
+            totalPages={props.totalPages}
+          />
+        )}
+      </span>
+      <Link className='grid' href={props.nextPageUrl}>
+        <Button
+          variant={'outline'}
+          className='size-6 p-0 disabled:opacity-25'
+          disabled={!props.hasNextPage}>
+          <ChevronRight className='size-4' />
+        </Button>
+      </Link>
+      {props.totalPages > 2 && (
+        <Link className='grid' href={props.lastPageUrl}>
+          <Button
+            variant={'outline'}
+            className='size-6 p-0 disabled:opacity-25'
+            disabled={!props.hasNextPage}>
+            <ChevronsRight className='size-4' />
+          </Button>
+        </Link>
+      )}
+    </div>
   );
 };
