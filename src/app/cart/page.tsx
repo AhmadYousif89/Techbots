dc0a9176761ff251b2fa50a2fe068ca3b27e71d8
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-29 02:40:16 +0300

    Refactor cart page component

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,9 +1,38 @@
-import { CartTabs } from '@/app/cart/_components/cart_tabs';
+import prisma from '@/lib/db';
+import { auth } from '@clerk/nextjs/server';
+import { CartViews } from '@/app/cart/_components/cart_view';
 
-export default function CartPage() {
+// export const getServerCart = async (userId: string) => {
+//   try {
+//     const cart = await prisma.cart.findUnique({
+//       where: { clerkUserId: userId },
+//       include: {
+//         cartItems: {
+//           include: {
+//             Product: true,
+//           },
+//         },
+//       },
+//     });
+//     return cart?.cartItems;
+//   } catch (error) {
+//     console.error(`Error getting cart items for userId: ${userId}\n`, error);
+//   }
+// };
+
+// export type ServerCartItems = NonNullable<Awaited<ReturnType<typeof getServerCart>>>;
+
+export default async function CartPage() {
+  // const { userId } = auth();
+
+  // let cart: ServerCartItems = [];
+  // if (userId) {
+  //   cart = (await getCart(userId)) ?? [];
+  // }
+
   return (
     <main className='min-h-svh'>
-      <CartTabs />
+      <CartViews />
     </main>
   );
 }
