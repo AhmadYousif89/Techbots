337a25be71304be6b7f102eb15922c2056ab1016
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-29 02:42:40 +0300

    Refactor product thumbnail component to improve code readability and maintainability

--- src/app/products/_components/product_thumpnail.tsx
+++ src/app/products/_components/product_thumpnail.tsx
@@ -1,16 +1,18 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { toast } from 'sonner';
-
+import { useAuth } from '@clerk/nextjs';
 import { Info, Trash2 } from 'lucide-react';
+import { capitalizeString } from '@/lib/utils';
 import { useCartMenuState, useWishlistMenuState } from '@/lib/store';
 
+import { TProduct } from '../_lib/types';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Card, CardTitle } from '@/components/ui/card';
+import { useCartStore } from '@/app/cart/_store/cart';
+import { removeFromServerCart } from '@/app/cart/_actions/actions';
 import { useLocalStorage } from '@/components/hooks/use_local_storage';
-import { TProduct } from '../_lib/types';
-import { capitalizeString } from '@/lib/utils';
 
 type ProductThumbnailProps = {
   product: TProduct;
@@ -20,54 +22,81 @@ type ProductThumbnailProps = {
 export function ProductThumbnail({ product, type }: ProductThumbnailProps) {
   const { setIsOpen: setIsCartOpen } = useCartMenuState();
   const { setIsOpen: setIsWishlistOpen } = useWishlistMenuState();
-  const { '1': setStoredItems } = useLocalStorage<TProduct[]>(type, []);
 
   return (
-    <Card className='flex items-center p-2'>
+    <Card className='flex items-center gap-2 p-2'>
       <Image
         src={product.mainImage}
         alt={product.title}
         width={80}
         height={80}
-        className='size-14 pr-2 object-contain'
+        className='size-14 px-2 object-contain'
       />
       <div className='flex items-center justify-between w-full'>
         <CardTitle className='flex flex-col text-sm mb-2 font-medium'>
           <div className='flex items-center gap-2 mb-2'>
-            <Badge variant='outline'>{capitalizeString(product.category)}</Badge>
-            <Badge variant='outline'>$ {product.price.toFixed(2)}</Badge>
+            <Badge
+              variant='outline'
+              className='text-muted-foreground font-semibold shadow-sm py-1'>
+              {capitalizeString(product.category)}
+            </Badge>
+            <Badge
+              variant='outline'
+              className='text-muted-foreground font-semibold shadow-sm py-1'>
+              ${product.price.toFixed(2)}
+            </Badge>
           </div>
           <Link
-            href={`/products/${product.asin}`}
-            className='text-xs text-muted-foreground hover:underline hover:text-blue-600'
+            href={`/products/${product.asin}?cat=${product.category}`}
+            className='ml-2 text-xs text-muted-foreground hover:underline hover:text-blue-500'
             onClick={() => {
               if (type === 'cart') setIsCartOpen(false);
-              if (type === 'wishlist') setIsWishlistOpen(false);
+              else setIsWishlistOpen(false);
             }}>
             {product.title.split(' ').slice(0, 4).join(' ')}
           </Link>
         </CardTitle>
 
-        <Button
-          variant={'destructive'}
-          title={type === 'cart' ? 'Remove from cart' : 'Remove from wishlist'}
-          className='size-6 p-1 bg-transparent hover:bg-destructive/20'
-          onClick={() => {
-            setStoredItems(items => items.filter(item => item.asin !== product.asin));
-            toast.custom(() => {
-              return (
-                <div className='flex items-center gap-4'>
-                  <Info className='text-blue-500' />
-                  <p className='text-sm'>
-                    Item removed from {type === 'cart' ? 'cart' : 'wishlist'}
-                  </p>
-                </div>
-              );
-            });
-          }}>
-          <Trash2 className='text-destructive' />
-        </Button>
+        <DeleteThumbnailButton product={product} type={type} />
       </div>
     </Card>
+  );
+}
+
+function DeleteThumbnailButton({ product, type }: ProductThumbnailProps) {
+  const { userId } = useAuth();
+  const removeFromCart = useCartStore(s => s.removeFromCart);
+  const { '1': setStoredItems } = useLocalStorage<TProduct[]>('wishlist', []);
+
+  const handleDelete = () => {
+    if (type === 'cart') {
+      removeFromCart(product.asin);
+      if (userId) {
+        removeFromServerCart(userId, product.asin, product.price);
+      }
+    } else {
+      setStoredItems(items => items.filter(item => item.asin !== product.asin));
+    }
+
+    toast.custom(() => {
+      return (
+        <div className='flex items-center gap-4'>
+          <Info className='text-blue-500' />
+          <p className='text-sm'>
+            Item removed from {type === 'cart' ? 'cart' : 'wishlist'}
+          </p>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <Button
+      variant={'destructive'}
+      title={type === 'cart' ? 'Remove from cart' : 'Remove from wishlist'}
+      className='size-6 p-1 bg-transparent hover:bg-destructive/20'
+      onClick={() => handleDelete()}>
+      <Trash2 className='text-destructive' />
+    </Button>
   );
 }
