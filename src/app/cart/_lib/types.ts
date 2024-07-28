80c8313dc6ded5a04d7fc884261749015e63e4c4
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-29 02:41:02 +0300

    Add cart, orders, and shipping form functionality

---
+++ src/app/cart/_lib/types.ts
@@ -0,0 +1,7 @@
+export type TCartItem = {
+  asin: string;
+  price: number;
+  cartQuantity: number;
+};
+
+export type ShippingType = 'free' | 'next';
--- src/app/cart/_store/shipping_form.ts
+++ src/app/cart/_store/shipping_form.ts
@@ -1,10 +1,21 @@
+import { z } from 'zod';
 import { create } from 'zustand';
-import { ShippingForm } from '../_components/cart_shipping';
 
-type ShippingFormState = {
-  data: ShippingForm;
-  setFormData: (data: ShippingForm | null) => void;
-};
+const validate = z.string().min(1, { message: 'Field is required' });
+
+const shippingSchema = z.object({
+  firstname: validate,
+  lastname: validate,
+  mainAddress: validate,
+  optionalAddress: z.string().optional(),
+  city: validate,
+  phone: z
+    .number({ coerce: true, message: 'Invalid phone number' })
+    .min(1, { message: 'Field is required' }),
+  shipping: z.union([z.literal('free'), z.literal('next')]),
+});
+export type ShippingForm = z.infer<typeof shippingSchema>;
+export type ShippingFormKeys = keyof ShippingForm;
 
 const initData: ShippingForm = {
   firstname: '',
@@ -13,13 +24,26 @@ const initData: ShippingForm = {
   optionalAddress: '',
   city: '',
   phone: 0,
-  shipping: 'free'
+  shipping: 'free',
 };
 
-export const useShippingFormStore = create<ShippingFormState>(set => ({
+type ShippingFormState = {
+  data: ShippingForm;
+  setFormData: (data: ShippingForm | null) => void;
+  formState: () => z.SafeParseReturnType<ShippingForm, ShippingForm>;
+};
+
+export const useShippingStore = create<ShippingFormState>((set, get) => ({
   data: initData,
   setFormData: data => {
     if (data) set({ data });
     else set({ data: initData });
-  }
+  },
+  formState: () => {
+    try {
+      return shippingSchema.safeParse(get().data);
+    } catch {
+      return shippingSchema.safeParse({} as ShippingForm);
+    }
+  },
 }));
