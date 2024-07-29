20ae5d5784a1cbb5e7cc22a24cd1bd33c8ae8a58
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-30 02:55:15 +0300

    Commented out unused fetchCart function

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -6,25 +6,25 @@ export const metadata = {
   description: 'Your shopping cart.',
 };
 
-export const getServerCart = async (userId: string) => {
-  try {
-    const cart = await prisma.cart.findUnique({
-      where: { clerkUserId: userId },
-      include: {
-        cartItems: {
-          include: {
-            Product: true,
-          },
-        },
-      },
-    });
-    return cart?.cartItems;
-  } catch (error) {
-    console.error(`Error getting cart items for userId: ${userId}\n`, error);
-  }
-};
+// export const fetchCart = async (userId: string) => {
+//   try {
+//     const cart = await prisma.cart.findUnique({
+//       where: { clerkUserId: userId },
+//       include: {
+//         cartItems: {
+//           include: {
+//             Product: true,
+//           },
+//         },
+//       },
+//     });
+//     return cart?.cartItems;
+//   } catch (error) {
+//     console.error(`Error getting cart items for userId: ${userId}\n`, error);
+//   }
+// };
 
-export type ServerCartItems = NonNullable<Awaited<ReturnType<typeof getServerCart>>>;
+// export type ServerCartItems = NonNullable<Awaited<ReturnType<typeof fetchCart>>>;
 
 export default async function CartPage() {
   return (
