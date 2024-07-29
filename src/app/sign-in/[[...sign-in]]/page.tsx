dbff0ab4d98bb6879a026cd0000562c6b2b362f8
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-07-30 02:34:10 +0300

    Remove sign-in and sign-up pages

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from '@clerk/nextjs';
-
-export default function Page() {
-  return (
-    <main className='my-52 grid place-content-center'>
-      <SignIn />;
-    </main>
-  );
-}
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from '@clerk/nextjs';
-
-export default function Page() {
-  return (
-    <main className='my-44 grid place-content-center'>
-      <SignUp />;
-    </main>
-  );
-}
