c0a2fb74560ad6614433d7b8d0a16522338f2e2d
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-08-02 16:01:12 +0300

    Remove loading component

--- src/app/products/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { ProductsViewSkeleton } from './_components/skeletons/products_view_skeleton';
-
-export default function Loading() {
-  return (
-    <div className='min-h-screen max-view mx-auto bg-background'>
-      <ProductsViewSkeleton />
-    </div>
-  );
-}
