3dce19305b912affd9db99e876f5e422e9dbe20e
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-08-03 04:53:43 +0300

    Update button and skeleton styles

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -8,7 +8,8 @@ const buttonVariants = cva(
   [
     'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors',
     'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
-    'disabled:pointer-events-none disabled:opacity-50'
+    'disabled:pointer-events-none disabled:opacity-50',
+    'touch-none select-none',
   ],
   {
     variants: {
@@ -19,19 +20,19 @@ const buttonVariants = cva(
           'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline'
+        link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
         default: 'h-10 px-4 py-2',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10'
-      }
+        icon: 'h-10 w-10',
+      },
     },
     defaultVariants: {
       variant: 'default',
-      size: 'default'
-    }
+      size: 'default',
+    },
   }
 );
 
--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -2,7 +2,7 @@ import { cn } from '@/lib/utils';
 
 function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className={cn('animate-pulse rounded-md bg-input/80', className)} {...props} />
+    <div className={cn('animate-pulse rounded-md bg-input', className)} {...props} />
   );
 }
 
