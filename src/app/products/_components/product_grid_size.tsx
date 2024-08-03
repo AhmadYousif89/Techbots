744bd3dc361ee1e21e7d39343afc931c963f6898
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-08-04 01:49:42 +0300

    Refactor product grid size component to use react hooks

--- src/app/products/_components/product_grid_size.tsx
+++ src/app/products/_components/product_grid_size.tsx
@@ -1,18 +1,21 @@
 'use client';
 
+import { useOptimistic, useTransition } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
+import { extractSearchParams } from '@/app/products/_lib/utils';
 
-import { Button } from '@/components/ui/button';
 import { Grid2x2 } from '@/icons/grid2x2';
 import { Grid3x3 } from '@/icons/grid3x3';
 import { Grid4x4 } from '@/icons/grid4x4';
-import { extractSearchParams } from '@/app/products/_lib/utils';
+import { Button } from '@/components/ui/button';
 
 export function ProductGridSize() {
   const router = useRouter();
   const params = useSearchParams();
-
   const sp = extractSearchParams(params.entries());
+  const [optimisticGrid, setOptimisticGrid] = useOptimistic(sp.grid);
+  const [isPending, startTransition] = useTransition();
+
   const newParams = new URLSearchParams({
     ...(sp.page && { page: sp.page }),
     ...(sp.category && { cat: sp.category }),
@@ -21,47 +24,42 @@ export function ProductGridSize() {
     ...(sp.min && { min: sp.min }),
     ...(sp.max && { max: sp.max }),
   });
-  const url = () => `/products/?${newParams.toString()}`;
+  const ps = newParams.toString();
+
+  const handleGridChange = (value: string) => {
+    startTransition(() => {
+      if (optimisticGrid === value) {
+        router.push(`/products?${ps}`);
+        return;
+      }
+      setOptimisticGrid(value);
+      router.push(`/products?${ps}&grid=${value}`);
+    });
+  };
 
   return (
-    <div role='group' className='hidden lg:flex items-center gap-1 col-span-full'>
+    <div
+      // data-pending={isPending ? '' : undefined}
+      className='hidden lg:flex items-center gap-1 col-span-full'>
       <Button
         variant='ghost'
-        data-active={sp.grid === '2'}
+        data-active={optimisticGrid === '2'}
         className='p-2 rounded-none ring-1 ring-transparent hover:bg-transparent data-[active="true"]:ring-input data-[active="true"]:shadow'
-        onClick={() => {
-          if (sp.grid === '2') {
-            router.push(url());
-            return;
-          }
-          router.push(url() + '&grid=2');
-        }}>
+        onClick={() => handleGridChange('2')}>
         <Grid2x2 />
       </Button>
       <Button
         variant='ghost'
-        data-active={sp.grid === '3'}
+        data-active={optimisticGrid === '3'}
         className='p-2 rounded-none ring-1 ring-transparent hover:bg-transparent data-[active="true"]:ring-input data-[active="true"]:shadow'
-        onClick={() => {
-          if (sp.grid === '3') {
-            router.push(url());
-            return;
-          }
-          router.push(url() + '&grid=3');
-        }}>
+        onClick={() => handleGridChange('3')}>
         <Grid3x3 />
       </Button>
       <Button
         variant='ghost'
-        data-active={sp.grid === '4'}
+        data-active={optimisticGrid === '4'}
         className='p-2 rounded-none ring-1 ring-transparent hover:bg-transparent data-[active="true"]:ring-input data-[active="true"]:shadow'
-        onClick={() => {
-          if (sp.grid === '4') {
-            router.push(url());
-            return;
-          }
-          router.push(url() + '&grid=4');
-        }}>
+        onClick={() => handleGridChange('4')}>
         <Grid4x4 />
       </Button>
     </div>
