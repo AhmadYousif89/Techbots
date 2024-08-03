b90e53cf2e3f1b372ce00ca2a8a4d71251d19485
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-08-04 01:47:15 +0300

    Refactor ProductsPage component structure

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -39,10 +39,16 @@ export default function ProductsPage({ searchParams }: PageProps) {
   const { category } = extractSearchParams(searchParams);
 
   return (
-    <main className='group min-h-screen max-view mx-auto bg-background'>
-      <Suspense fallback={<ProductsViewSkeleton />}>
+    <Suspense fallback={<ProductsViewSkeleton />}>
+      <main className='group min-h-screen max-view mx-auto bg-background'>
         <div className='px-4 md:px-10 flex items-center justify-between h-14 bg-muted'>
           <BreadcrumbSection category={category} />
+          <div className='hidden relative hover:bg-input rounded-full overflow-hidden xl:flex items-center min-w-80 h-8 ring-2 ring-input *:w-full *:h-full *:justify-end *:pr-2'>
+            <p className='absolute z-0 top-0 left-0 pl-4 pointer-events-none text-xs text-muted-foreground font-semibold flex items-center'>
+              <span className='w-full'>Search...</span>
+            </p>
+            <SearchProducts />
+          </div>
           <PaginationSummary searchParams={searchParams} />
         </div>
 
@@ -57,18 +63,18 @@ export default function ProductsPage({ searchParams }: PageProps) {
         </div>
         {/* End Filter */}
 
-        <div className='xl:grid xl:gap-4 xl:grid-cols-[minmax(20%,auto),1fr] pb-8'>
+        <div className='xl:grid xl:gap-8 xl:grid-cols-[minmax(20%,auto),1fr] py-8'>
           {/* Filter Section On Desktop */}
-          <div className='relative hidden xl:flex self-start justify-between pt-6'>
+          <div className='relative hidden xl:flex self-start justify-between'>
             <FilterProducts searchParams={searchParams} open='filter' />
-            <div className='absolute right-0 top-9 flex items-center gap-4'>
+            <div className='absolute right-0 top-3 flex items-center gap-4'>
               <SortProducts searchParams={searchParams} />
             </div>
           </div>
           {/* End Filter */}
           <ProductGrid searchParams={searchParams} />
         </div>
-      </Suspense>
-    </main>
+      </main>
+    </Suspense>
   );
 }
