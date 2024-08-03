e1af9d277dbb7d2dd2b1d57b58765b754bfec74e
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-08-04 01:51:32 +0300

    Refactor GridItemsSkeleton component

--- src/app/products/_components/skeletons/grid_item.tsx
+++ src/app/products/_components/skeletons/grid_item.tsx
@@ -1,16 +1,23 @@
+import { cn } from '@/lib/utils';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Card, CardFooter, CardHeader } from '@/components/ui/card';
 
-export function GridItemsSkeleton() {
+export function GridItemsSkeleton({ grid }: { grid: string }) {
   return (
-    <div className='mt-8 grid gap-8 grid-cols-[repeat(auto-fit,minmax(160px,1fr))] md:grid-cols-[repeat(auto-fit,minmax(180px,1fr))] xl:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] col-span-full'>
+    <>
       {Array.from({ length: 8 }).map((_, i) => (
         <Card
           key={i}
-          className='grid justify-self-center py-2 px-4 rounded shadow-none border-0 ring-offset-4 hover:ring-1 hover:ring-muted'>
-          <Skeleton className='size-32 mb-8 mx-auto bg-input/50' />
-          <Skeleton className='h-1 w-full bg-input/75' />
-          <CardHeader className='p-0 py-8'>
+          className='grid auto-rows-[150px_3px_1fr_3px_auto] md:auto-rows-[200px_3px_1fr_3px_auto] justify-self-center py-2 px-4 rounded shadow-none border-0 max-w-xs ring-offset-4 hover:ring-1 hover:ring-muted'>
+          <Skeleton
+            className={cn(
+              'size-28 place-self-center',
+              grid === '3' && 'lg:size-36 p-0',
+              grid === '2' && 'lg:size-44 p-2'
+            )}
+          />
+          <Skeleton className='h-px w-full bg-input/75' />
+          <CardHeader className='p-0 py-8 min-w-40 xl:min-w-52'>
             <Skeleton className='h-2 w-full bg-input/50' />
             <Skeleton className='h-2 w-full bg-input/50' />
             <Skeleton className='h-2 w-full bg-input/50' />
@@ -18,13 +25,13 @@ export function GridItemsSkeleton() {
             <Skeleton className='h-2 w-full bg-input/50' />
             <Skeleton className='h-2 w-1/2 bg-input/50' />
           </CardHeader>
-          <Skeleton className='h-1 w-full bg-input/75' />
-          <CardFooter className='p-0 pt-4 gap-4 justify-between'>
+          <Skeleton className='h-px w-full bg-input/75 mb-2' />
+          <CardFooter className='p-0 gap-4 justify-between'>
             <Skeleton className='rounded w-20 h-8 bg-input/50' />
             <Skeleton className='rounded-full size-7 bg-input/50' />
           </CardFooter>
         </Card>
       ))}
-    </div>
+    </>
   );
 }
