2b2334b8e081bf7478b71a6e51f51a851af48122
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-08-03 04:52:28 +0300

    Update import statement in pagination_summary.tsx

--- src/app/products/_components/pagination_summary.tsx
+++ src/app/products/_components/pagination_summary.tsx
@@ -2,7 +2,7 @@ import prisma from '@/lib/db';
 import { cn } from '@/lib/utils';
 import { SearchParams } from '../_lib/types';
 import { extractSearchParams } from '../_lib/utils';
-import { getFilters } from '../product_grid';
+import { getFilters } from '../grid';
 
 type PaginationSummaryProps = {
   searchParams: SearchParams;
