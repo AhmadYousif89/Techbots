ddbdba2be0687876e9e8c307ce06f0d888047a81
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-08-05 01:52:56 +0300

    Refactor cart page component

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,34 +1,13 @@
-import prisma from '@/lib/db';
-import { CartViews } from '@/app/cart/_components/cart_view';
+import { CartViews } from "@/app/cart/_components/cart_view";
 
 export const metadata = {
-  title: 'Cart',
-  description: 'Your shopping cart.',
+  title: "Cart",
+  description: "Your shopping cart.",
 };
-
-// export const fetchCart = async (userId: string) => {
-//   try {
-//     const cart = await prisma.cart.findUnique({
-//       where: { clerkUserId: userId },
-//       include: {
-//         cartItems: {
-//           include: {
-//             Product: true,
-//           },
-//         },
-//       },
-//     });
-//     return cart?.cartItems;
-//   } catch (error) {
-//     console.error(`Error getting cart items for userId: ${userId}\n`, error);
-//   }
-// };
-
-// export type ServerCartItems = NonNullable<Awaited<ReturnType<typeof fetchCart>>>;
 
 export default async function CartPage() {
   return (
-    <main className='min-h-svh'>
+    <main className="min-h-svh">
       <CartViews />
     </main>
   );
