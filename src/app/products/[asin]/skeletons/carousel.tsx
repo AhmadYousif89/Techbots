3b32cd6bbf88141f9e10e00154d3a74c6d33504d
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-08-05 01:56:02 +0300

    Refactor carousel skeleton component

--- src/app/products/[asin]/skeletons/carousel.tsx
+++ src/app/products/[asin]/skeletons/carousel.tsx
@@ -2,31 +2,33 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from '@/components/ui/carousel';
-import { Skeleton } from '@/components/ui/skeleton';
+} from "@/components/ui/carousel";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export function CarouselSkeleton() {
   return (
-    <div className='min-h-[600px] self-start pt-10 pb-16'>
-      <div className='mx-4'>
-        <Skeleton className='min-h-96 w-full bg-input/70' />
-        <Skeleton className='w-full h-1 my-8 bg-input' />
+    <div className="min-h-[600px] self-start pb-16 pt-10">
+      <div className="mx-4">
+        <Skeleton className="min-h-96 w-full bg-input/70" />
+        <Skeleton className="my-8 h-1 w-full bg-input" />
       </div>
       <Carousel
-        className='max-w-screen-md mx-auto'
-        opts={{ dragFree: true, align: 'start' }}>
-        <CarouselContent className='px-4'>
+        className="mx-auto max-w-screen-md"
+        opts={{ dragFree: true, align: "start" }}
+      >
+        <CarouselContent className="px-4">
           {Array.from({ length: 8 }).map((image, index) => (
-            <CarouselItem key={index} className='basis-32 sm:basis-36 lg:basis-40'>
-              <Skeleton className='min-h-28'></Skeleton>
+            <CarouselItem
+              key={index}
+              className="basis-32 sm:basis-36 lg:basis-40"
+            >
+              <Skeleton className="min-h-28"></Skeleton>
             </CarouselItem>
           ))}
         </CarouselContent>
-        <div className='flex justify-between mt-4 px-4'>
-          <Skeleton className='rounded-full size-10' />
-          <Skeleton className='rounded-full size-10' />
+        <div className="mt-4 flex justify-between px-4">
+          <Skeleton className="size-10 rounded-full" />
+          <Skeleton className="size-10 rounded-full" />
         </div>
       </Carousel>
     </div>
