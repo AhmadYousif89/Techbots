033af54571d9f8bf51cb94c11ef1488d1ff87b81
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-08-26 22:13:16 +0300

    Add Loader2 component to ProductGridSize

--- src/app/(public)/products/_components/product_grid_size.tsx
+++ src/app/(public)/products/_components/product_grid_size.tsx
@@ -3,6 +3,7 @@
 import { useOptimistic, useTransition } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { extractSearchParams } from "@/app/lib/utils";
+import { Loader2 } from "lucide-react";
 
 import { Grid2x2 } from "@/icons/grid2x2";
 import { Grid3x3 } from "@/icons/grid3x3";
@@ -33,15 +34,12 @@ export function ProductGridSize() {
         return;
       }
       setOptimisticGrid(value);
-      router.push(`/products?${ps}&grid=${value}`);
+      router.push(`/products?${ps && ps + "&"}grid=${value}`);
     });
   };
 
   return (
-    <div
-      // data-pending={isPending ? '' : undefined}
-      className="col-span-full hidden items-center gap-1 lg:flex"
-    >
+    <div className="col-span-full hidden items-center gap-1 lg:flex">
       <Button
         variant="ghost"
         data-active={optimisticGrid === "2"}
@@ -66,6 +64,12 @@ export function ProductGridSize() {
       >
         <Grid4x4 />
       </Button>
+
+      {isPending && (
+        <div className="ml-4 p-1">
+          <Loader2 className="size-5 animate-[spin_1s_linear_infinite]" />
+        </div>
+      )}
     </div>
   );
 }
