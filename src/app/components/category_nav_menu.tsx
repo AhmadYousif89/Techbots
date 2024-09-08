988bcc7c6d5e9796642ec242f9a48ccddb90e1f2
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-09-09 00:51:34 +0300

    Refactor category navigation menu component

--- src/app/components/category_nav_menu.tsx
+++ src/app/components/category_nav_menu.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { use } from "react";
-import Link from "next/link";
-import { useSearchParams } from "next/navigation";
-import { capitalizeString } from "@/app/lib/utils";
+import { use, useRef } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { capitalizeString, extractSearchParams } from "@/app/lib/utils";
 import { cn } from "@/lib/utils";
 
 import {
@@ -11,10 +10,10 @@ import {
   AccordionItem,
   AccordionContent,
   AccordionTrigger,
-} from "../../components/ui/accordion";
-import { Card } from "../../components/ui/card";
-import { Button } from "../../components/ui/button";
-import { extractSearchParams } from "@/app/lib/utils";
+} from "@/components/ui/accordion";
+import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { useOnClickOutside } from "./hooks/use_onClick_outside";
 
 type Props = {
   data: Promise<string[] | undefined>;
@@ -23,6 +22,7 @@ type Props = {
 export function CategoryNavMenu({ data }: Props) {
   let categories = use(data) ?? [];
   categories = categories.filter((cat) => !cat.startsWith("smart"));
+  const router = useRouter();
   const params = useSearchParams();
   const sp = extractSearchParams(params.entries());
   const newParams = new URLSearchParams({
@@ -36,52 +36,64 @@ export function CategoryNavMenu({ data }: Props) {
   const ps = newParams.toString() ? `&${newParams.toString()}` : "";
   const end = 14;
   const cats = categories.length > end ? categories.slice(0, end) : categories;
-  const url = (cat: string) => `/products/?cat=${cat}${ps}`;
+  const url = (cat: string) => `/products?cat=${cat}${ps}`;
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = () => {
+    if (ref.current) {
+      const trigger = ref.current.querySelector('[aria-expanded="true"]');
+      if (trigger) (trigger as HTMLButtonElement).click();
+    }
+  };
+
+  useOnClickOutside(ref, handleClickOutside);
+
+  const renderCategoryList = (categories: string[]) => (
+    <>
+      {categories.map((category) => (
+        <Button
+          key={category}
+          variant={"link"}
+          onClick={() => {
+            if (category === sp.category) {
+              router.push("/products");
+            } else {
+              router.push(url(category));
+            }
+          }}
+          aria-selected={category === sp.category}
+          className="px-0 text-xs text-secondary aria-selected:text-blue-400 aria-selected:underline"
+        >
+          <span>{capitalizeString(category)}</span>
+        </Button>
+      ))}
+    </>
+  );
 
   return (
     <Accordion type="single" collapsible>
-      <AccordionItem value="cats-1" className="group border-b-0">
+      <AccordionItem ref={ref} value="cats-1" className="group border-b-0">
         <AccordionTrigger className="gap-1 text-xs text-secondary underline-offset-4">
           Categories
         </AccordionTrigger>
         <Card
           className={cn(
-            "absolute left-1/2 top-[100%] -translate-x-1/2",
+            "absolute left-1/2 top-full -translate-x-1/2",
             "w-full bg-primary/85 backdrop-blur-sm",
             'max-view rounded-none border-0 group-data-[state="open"]:border-t',
           )}
         >
           <AccordionContent className="max-view mx-auto grid p-2">
             <div className="flex items-center justify-evenly">
-              {cats.map((category) => (
-                <Button
-                  asChild
-                  key={category}
-                  variant={"link"}
-                  aria-selected={category === params.get("cat")}
-                  className="px-0 text-xs text-secondary aria-selected:text-blue-400 aria-selected:underline"
-                >
-                  <Link href={url(category)}>{capitalizeString(category)}</Link>
-                </Button>
-              ))}
+              {renderCategoryList(cats)}
             </div>
             {categories.length > end && (
               <Accordion type="single" collapsible>
                 <AccordionItem value="cats-2" className="border-b-0">
                   <AccordionContent>
                     <div className="flex flex-wrap items-center justify-center gap-x-12">
-                      {categories.slice(end).map((category) => (
-                        <Button
-                          asChild
-                          key={category}
-                          variant={"link"}
-                          className="px-0 text-xs text-secondary"
-                        >
-                          <Link href={url(category)}>
-                            {capitalizeString(category)}
-                          </Link>
-                        </Button>
-                      ))}
+                      {renderCategoryList(categories.slice(end))}
                     </div>
                   </AccordionContent>
                   <AccordionTrigger className="justify-center gap-1 p-0 pb-2 text-xs text-secondary underline-offset-4"></AccordionTrigger>
