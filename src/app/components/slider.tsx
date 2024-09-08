9740a56f1a0fa67ef659b6890f2c0ecd71b4636a
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-09-09 00:53:12 +0300

    Update slider component with new image and styling

--- src/app/components/slider.tsx
+++ src/app/components/slider.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import Image from "next/image";
 import Autoplay from "embla-carousel-autoplay";
 
-import { Button } from "@/components/ui/button";
 import {
   Carousel,
   CarouselContent,
@@ -40,7 +39,7 @@ const slides = [
   {
     url: "/products?cat=accessories",
     title: "Get the best gaming experience with our latest gaming accessories",
-    img: "https://m.media-amazon.com/images/I/618zZ7u3sUL.jpg",
+    img: "https://m.media-amazon.com/images/I/61mB8mL33pL.jpg",
   },
 ];
 
@@ -48,8 +47,8 @@ export function HomeSlider() {
   return (
     <Carousel
       opts={{ loop: true }}
-      plugins={[Autoplay({ delay: 6000, stopOnInteraction: false })]}
-      className="group relative"
+      plugins={[Autoplay({ delay: 6000 })]}
+      className="group"
     >
       <CarouselContent className="h-[calc(100svh-80px)]">
         {slides.map((slide, index) => (
@@ -58,30 +57,32 @@ export function HomeSlider() {
             className="grid grid-rows-2 pl-1 md:grid-cols-2"
           >
             <Image
-              width={600}
-              height={600}
+              width={900}
+              height={900}
               loading="eager"
               src={slide.img}
-              alt={"product image"}
-              className="max-view size-full bg-background object-cover md:col-[2] md:row-span-full md:object-contain"
+              alt={slide.title}
+              className="size-full bg-background object-contain px-8 md:col-[2] md:row-span-full"
             />
-            <div className="grid place-content-center gap-4 overflow-hidden bg-primary px-4 py-8 text-center backdrop-blur-sm sm:gap-8 md:row-span-full lg:gap-12 lg:px-16">
-              <p className="rounded p-4 text-lg text-secondary lg:text-xl xl:text-2xl">
+            <div className="grid place-content-center gap-4 overflow-hidden bg-primary px-4 py-8 text-center backdrop-blur-sm md:row-span-full lg:px-16">
+              <p className="max-w-sm text-pretty rounded bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text p-4 px-2 text-lg text-transparent lg:max-w-xl lg:text-2xl xl:text-4xl">
                 {slide.title}
               </p>
-              <Button
-                size={"lg"}
-                variant={"outline"}
-                className="place-self-center bg-foreground/50 text-background"
-              >
-                <Link href={slide.url}>Shop Now</Link>
-              </Button>
+              <button className="group relative h-12 w-36 select-none place-self-center overflow-hidden rounded-full p-[2px] shadow-lg shadow-muted/5 active:outline-dashed active:outline-2 active:outline-offset-4 active:outline-muted/50 lg:active:outline-1">
+                <Link
+                  href={slide.url}
+                  className="inline-flex size-full items-center justify-center rounded-full bg-primary bg-gradient-to-b from-black to-secondary/10 text-sm font-medium text-secondary"
+                >
+                  Shop Now
+                </Link>
+                <div className="absolute inset-0 -top-5 -z-10 aspect-square h-20 w-full origin-center bg-gradient-to-r from-blue-500 to-pink-500" />
+              </button>
             </div>
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious className="left-5 z-10 hidden size-12 border-input/20 bg-muted *:text-primary group-hover:inline-flex lg:h-1/2 lg:rounded lg:*:size-5" />
-      <CarouselNext className="right-5 z-10 hidden size-12 border-input/20 bg-muted *:text-primary group-hover:inline-flex lg:h-1/2 lg:rounded lg:*:size-5" />
+      <CarouselPrevious className="left-2 hidden size-12 border-2 border-primary/25 bg-muted shadow *:text-primary group-hover:inline-flex lg:h-1/2 lg:rounded lg:*:size-5" />
+      <CarouselNext className="right-2 hidden size-12 border-2 border-primary/25 bg-muted shadow *:text-primary group-hover:inline-flex lg:h-1/2 lg:rounded lg:*:size-5" />
     </Carousel>
   );
 }
