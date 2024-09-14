d838121e146aee4477502f494f82c167358290e3
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-09-14 17:38:54 +0300

    Add a custom log formatter class to customize the format for diffirent levels

--- logger.py
+++ logger.py
@@ -2,30 +2,38 @@ import logging
 import os
 
 
-def setup_logger(logger_name, log_file, level=logging.INFO):
+def setup_logger(
+    logger_name,
+    log_file,
+    formatr='%(levelname)s: %(asctime)s - %(message)s',
+    info_formatr='%(levelname)s: %(message)s',
+) -> logging.Logger:
     if not os.path.exists(os.path.dirname(log_file)):
         os.makedirs(os.path.dirname(log_file))
 
-    with open(log_file, 'a') as f:
-        f.write('=' * 100 + '\n')
-
     logger = logging.getLogger(logger_name)
-    logger.setLevel(level)
+    logger.setLevel(logging.INFO)
 
-    # Create a file handler
+    # Create handlers for the logger
     log_file_handler = logging.FileHandler(log_file)
-    log_file_handler.setLevel(level)
-
-    # Create a console handler
+    log_file_handler.setLevel(logging.INFO)
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(level)
+    console_handler.setLevel(logging.INFO)
 
-    # Create a logging format
-    formatter = logging.Formatter('%(levelname)s: %(message)s')
-    log_file_handler.setFormatter(formatter)
-    console_handler.setFormatter(formatter)
+    # Define a single formatter function based on log level
+    class CustomFormatter(logging.Formatter):
+        def format(self, record):
+            if record.levelno == logging.INFO:
+                self._style._fmt = info_formatr
+            else:
+                self._style._fmt = formatr
+            return super().format(record)
 
-    # Add the handlers to the logger
+    custom_formatter = CustomFormatter(formatr, datefmt='%Y-%m-%d %H:%M:%S')
+    log_file_handler.setFormatter(custom_formatter)
+    console_handler.setFormatter(custom_formatter)
+
+    # Add handlers to the logger
     logger.addHandler(log_file_handler)
     logger.addHandler(console_handler)
 
