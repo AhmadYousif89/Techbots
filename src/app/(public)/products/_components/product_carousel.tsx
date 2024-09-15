5bd183468c361ede07c4f9e00178ca6d4a76de8b
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-09-16 02:39:09 +0300

    Refactor product carousel component to use blob images and optimize image loading

--- src/app/(public)/products/_components/product_carousel.tsx
+++ src/app/(public)/products/_components/product_carousel.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
-import { cn } from "@/lib/utils";
+import { use, useMemo, useState, useCallback } from "react";
 import { TProduct } from "@/app/lib/types";
+import { cn } from "@/lib/utils";
 
 import {
   Carousel,
@@ -20,8 +20,40 @@ import {
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
-export function ProductCarousel(product: TProduct) {
+type ProductCarouselProps = {
+  blobImages?: Promise<Record<string, { asin: string; data: any }[]>>;
+  product: TProduct;
+};
+
+const convertToBlob = (uri: string) => {
+  const mimeType = uri.split(",")[0].split(":")[1].split(";")[0];
+  const base64Data = uri.split(",")[1];
+  const buffer = Buffer.from(base64Data, "base64");
+
+  return new Blob([buffer], { type: mimeType });
+};
+
+export function ProductCarousel({ product, blobImages }: ProductCarouselProps) {
   const [imageIndex, setImageIndex] = useState(0);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const thumbImages = blobImages ? use(blobImages) : null;
+
+  const imageUrl = useMemo(() => {
+    if (!thumbImages || Object.keys(thumbImages).length < 1) return "";
+
+    const images = thumbImages[product.category];
+    if (!images) return "";
+
+    const image = images.find((img) => img.asin === product.asin);
+    return image ? URL.createObjectURL(convertToBlob(image.data)) : "";
+  }, [thumbImages, product.category, product.asin]);
+
+  const handleImageLoad = useCallback(() => setIsLoaded(true), []);
+
+  const handleImageIndexChange = useCallback(
+    (index: number) => setImageIndex(index),
+    [],
+  );
 
   return (
     <ResizablePanelGroup
@@ -37,9 +69,23 @@ export function ProductCarousel(product: TProduct) {
           <DialogTrigger className="relative flex size-full max-lg:pointer-events-none lg:mx-auto lg:max-w-lg">
             <Image
               fill
+              src={imageUrl}
+              alt={`thumbnail image for ${product.title}`}
+              className={cn(
+                "size-full animate-pulse object-contain",
+                isLoaded && "hidden",
+              )}
+            />
+            <Image
+              fill
+              loading="lazy"
               alt={product.title}
               src={product.images[imageIndex].link}
-              className="object-contain p-8"
+              className={cn(
+                "object-contain p-8 transition-opacity duration-300",
+                isLoaded ? "opacity-100" : "opacity-0",
+              )}
+              onLoad={handleImageLoad}
             />
           </DialogTrigger>
           <DialogContent className="h-[calc(100%-15vh)] max-w-[calc(100%-15vw)]">
@@ -62,12 +108,12 @@ export function ProductCarousel(product: TProduct) {
             {product.images.map((image, index) => (
               <CarouselItem key={index} className="basis-[100px]">
                 <Button
-                  variant={"ghost"}
+                  variant="ghost"
                   className={cn(
                     "h-24 w-full p-0",
                     imageIndex === index && "border shadow-sm",
                   )}
-                  onClick={() => setImageIndex(index)}
+                  onClick={() => handleImageIndexChange(index)}
                 >
                   <Image
                     src={image.link}
