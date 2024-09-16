4d99f18419d3c1a6339769ea206fa733f7eb061d
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-09-17 01:05:08 +0300

    Refactor search functionality and add search results

--- src/app/(public)/products/search.tsx
+++ src/app/(public)/products/search.tsx
@@ -1,10 +1,12 @@
+"use client";
+
+import { use } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { Suspense } from "react";
-
-import prisma from "@/app/lib/db";
-import { cache } from "@/app/lib/cache";
 import { Search } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { Data } from "@/app/lib/products";
 import { capitalizeString } from "@/app/lib/utils";
 
 import {
@@ -16,55 +18,14 @@ import {
   CommandList,
 } from "@/components/ui/command";
 import { Button } from "@/components/ui/button";
-import { Category } from "@/app/lib/types";
-import { Skeleton } from "@/components/ui/skeleton";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
-type Data = {
-  asin: string;
-  title: string;
-  mainImage: string;
-  category: Category;
+type SearchProductProps = {
+  data: Promise<Record<string, Data[]>>;
 };
 
-const day = 60 * 60 * 24;
-
-const getSearchedProducts = cache(
-  async () => {
-    return prisma.product.findMany({
-      select: {
-        asin: true,
-        title: true,
-        category: true,
-        mainImage: true,
-      },
-    }) as Promise<Data[]>;
-  },
-  ["/products", "getSearchedProducts"],
-  { revalidate: day },
-);
-
-export async function SearchProducts() {
-  let categories: Category[] = [];
-  let itemsByCategory: Record<string, Data[]> = {};
-  try {
-    const data = await getSearchedProducts();
-
-    categories = [...new Set(data.map((i) => i.category))];
-    itemsByCategory = data.reduce((list: Record<string, Data[]>, p) => {
-      const item: Data = {
-        asin: p.asin,
-        title: p.title,
-        mainImage: p.mainImage,
-        category: p.category as Category,
-      };
-      list[p.category] = [...(list[p.category] ?? []), item];
-      return list;
-    }, {});
-  } catch (error) {
-    console.error(error);
-    throw new Error("Failed to fetch search data!");
-  }
+export function SearchProducts({ data }: SearchProductProps) {
+  const itemsByCategory = use(data);
 
   return (
     <Dialog>
@@ -81,45 +42,75 @@ export async function SearchProducts() {
         </Button>
       </DialogTrigger>
       <DialogContent className="p-4">
-        <Command>
+        <Command
+          loop
+          filter={(value, search) => {
+            if (value.toLowerCase().includes(search.toLowerCase())) return 1;
+            return 0;
+          }}
+        >
           <CommandInput placeholder="Type something to search..." />
+          <CommandEmpty className="py-8 text-center text-sm font-semibold text-muted-foreground">
+            No results found.
+          </CommandEmpty>
           <CommandList>
-            <CommandEmpty>No results found.</CommandEmpty>
-            {categories.map((category, i) => (
-              <Suspense
-                key={i}
-                fallback={<Skeleton className="h-2 w-20 bg-input" />}
+            {Object.keys(itemsByCategory).map((category, i) => (
+              <CommandGroup
+                key={i + "-" + category}
+                className="[&_[cmdk-group-heading]]:px-0"
+                heading={
+                  <h3 className="text-xs font-medium text-muted-foreground">
+                    {capitalizeString(category)}
+                  </h3>
+                }
               >
-                <CommandGroup
-                  key={i + "-" + category}
-                  className="pb-4"
-                  heading={capitalizeString(category)}
-                >
-                  {itemsByCategory[category].map((item, i) => (
-                    <CommandItem key={i} className="my-2 cursor-pointer !p-0">
-                      <Link
-                        href={`/products/${item.asin}`}
-                        className="flex items-center gap-4"
-                      >
-                        <Image
-                          src={item.mainImage}
-                          alt={item.title}
-                          width={48}
-                          height={48}
-                          className="size-12"
-                        />
-                        <p className="text-xs font-medium text-muted-foreground">
-                          {item.title.split(" ").slice(0, 8).join(" ")}
-                        </p>
-                      </Link>
-                    </CommandItem>
-                  ))}
-                </CommandGroup>
-              </Suspense>
+                <SearchItems items={itemsByCategory[category]} />
+              </CommandGroup>
             ))}
           </CommandList>
         </Command>
       </DialogContent>
     </Dialog>
+  );
+}
+
+type SearchResultsProps = {
+  items: Data[];
+};
+
+function SearchItems({ items }: SearchResultsProps) {
+  const router = useRouter();
+
+  return (
+    <>
+      {items.map((item, i) => (
+        <CommandItem
+          key={i}
+          className="my-2 cursor-pointer !p-0"
+          onSelect={() => {
+            router.push(`/products/${item.asin}`);
+          }}
+        >
+          <Link
+            href={`/products/${item.asin}`}
+            className="flex items-center gap-4"
+          >
+            <Image
+              src={item.mainImage}
+              alt={item.title}
+              width={50}
+              height={50}
+              className="size-10 object-contain"
+            />
+            <p
+              className="line-clamp-1 pr-4 text-xs font-medium text-muted-foreground"
+              title={item.title}
+            >
+              {item.title}
+            </p>
+          </Link>
+        </CommandItem>
+      ))}
+    </>
   );
 }
