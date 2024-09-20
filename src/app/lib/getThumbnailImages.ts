ae6792063690ff023e13397acae4d1da123a2504
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-09-21 00:44:25 +0300

    Add getThumbnailImages function to retrieve thumbnail images from JSON data

---
+++ src/app/lib/getThumbnailImages.ts
@@ -0,0 +1,26 @@
+import { readFile } from "fs/promises";
+
+type ImageData = { asin: string; data: string };
+type Base64ImgList = { asin: string }[];
+
+export async function getThumbnailImages() {
+  try {
+    const data = await readFile("data/thumbnail_data_uri.json", "utf8");
+    const uriObject = JSON.parse(data) as Record<string, Base64ImgList>;
+
+    const categories: Record<string, ImageData[]> = {};
+    for (const [type, imageData] of Object.entries(uriObject)) {
+      const images: ImageData[] = [];
+      for (const image of imageData) {
+        const [asin, data] = Object.entries(image)[0];
+        images.push({ asin, data });
+      }
+      categories[type] = images;
+    }
+
+    return categories;
+  } catch (error) {
+    console.error(error);
+    return {};
+  }
+}
