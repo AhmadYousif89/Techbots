0f2923bb171810d84391ba611948f783785d6908
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-09-21 00:51:14 +0300

    Refactor component to fetch list of categories from the DB

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -7,14 +7,16 @@ import { SideMenu } from "./side_menu";
 import { CartMenu } from "./cart/cart_menu";
 import { WishListMenu } from "./wishlist/wishlist_menu";
 import { UserProfileButton } from "./user_profile_button";
+import { getCategories } from "../lib/products";
 
 export function Header() {
   const { userId } = auth();
+  const categories = getCategories();
 
   return (
     <header className="sticky top-0 z-20 h-20 bg-foreground/85 px-8 backdrop-blur-sm">
       <div className="max-view mx-auto flex h-full items-center justify-between">
-        <SideMenu />
+        <SideMenu categoryList={categories} />
         <Logo />
         <MainNav />
         <div className="flex items-center gap-4 lg:gap-8">
