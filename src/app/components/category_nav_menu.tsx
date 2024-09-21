a4ca0cd1a9946f249b7d2f419aac3f3977fe6df9
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-09-21 22:59:22 +0300

    Refactor the newParams object and update searchParams cookie

--- src/app/components/category_nav_menu.tsx
+++ src/app/components/category_nav_menu.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { use, useRef } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { capitalizeString, extractSearchParams } from "@/app/lib/utils";
+import { use, useRef, useTransition, useOptimistic } from "react";
+
 import { cn } from "@/lib/utils";
+import { capitalizeString, extractSearchParams } from "@/app/lib/utils";
 
 import {
   Accordion,
@@ -14,55 +15,62 @@ import {
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { useOnClickOutside } from "./hooks/use_onClick_outside";
+import { updateCookies } from "../lib/update_cookies";
 
 type Props = {
-  data: Promise<string[] | undefined>;
+  data: Promise<string[]>;
 };
 
 export function CategoryNavMenu({ data }: Props) {
-  let categories = use(data) ?? [];
-  categories = categories.filter((cat) => !cat.startsWith("smart"));
   const router = useRouter();
   const params = useSearchParams();
   const sp = extractSearchParams(params.entries());
-  const newParams = new URLSearchParams({
-    ...(sp.page && { page: sp.page }),
-    ...(sp.brand && { brand: sp.brand }),
-    ...(sp.sort && { sort: sp.sort }),
-    ...(sp.min && { min: sp.min }),
-    ...(sp.max && { max: sp.max }),
-    ...(sp.grid && { grid: sp.grid }),
-  });
+  const [_, startTransition] = useTransition();
+  const [optCategory, setOptCategory] = useOptimistic(sp.category);
+
+  const newParams = new URLSearchParams(
+    Object.entries(sp).filter(([k, v]) => (v ? v && k !== "category" : v)),
+  );
   const ps = newParams.toString() ? `&${newParams.toString()}` : "";
   const end = 14;
-  const cats = categories.length > end ? categories.slice(0, end) : categories;
+  const categories = use(data);
+  const cats = categories.slice(0, end);
   const url = (cat: string) => `/products?cat=${cat}${ps}`;
 
   const ref = useRef<HTMLDivElement>(null);
 
   const handleClickOutside = () => {
-    if (ref.current) {
-      const trigger = ref.current.querySelector('[aria-expanded="true"]');
-      if (trigger) (trigger as HTMLButtonElement).click();
-    }
+    const trigger = ref.current?.querySelector(
+      '[aria-expanded="true"]',
+    ) as HTMLButtonElement;
+    trigger?.click();
   };
 
   useOnClickOutside(ref, handleClickOutside);
+
+  const handleCategoryChange = (category: string) => {
+    setOptCategory(category);
+
+    startTransition(() => {
+      if (category === optCategory) {
+        setOptCategory("");
+        router.push("/products");
+        updateCookies({ cat: undefined });
+        return;
+      }
+      router.push(url(category));
+      updateCookies({ cat: category });
+    });
+  };
 
   const renderCategoryList = (categories: string[]) => (
     <>
       {categories.map((category) => (
         <Button
           key={category}
-          variant={"link"}
-          onClick={() => {
-            if (category === sp.category) {
-              router.push("/products");
-            } else {
-              router.push(url(category));
-            }
-          }}
-          aria-selected={category === sp.category}
+          variant="link"
+          onClick={() => handleCategoryChange(category)}
+          aria-selected={category === optCategory}
           className="px-0 text-xs text-secondary aria-selected:text-blue-400 aria-selected:underline"
         >
           <span>{capitalizeString(category)}</span>
@@ -96,7 +104,7 @@ export function CategoryNavMenu({ data }: Props) {
                       {renderCategoryList(categories.slice(end))}
                     </div>
                   </AccordionContent>
-                  <AccordionTrigger className="justify-center gap-1 p-0 pb-2 text-xs text-secondary underline-offset-4"></AccordionTrigger>
+                  <AccordionTrigger className="justify-center gap-1 p-0 pb-2 text-xs text-secondary underline-offset-4" />
                 </AccordionItem>
               </Accordion>
             )}
